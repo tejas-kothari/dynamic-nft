6f4a4f4d03496f237263cb97d917c0b2ced62203
Tejas Kothari <tejas23682320@gmail.com>
2021-10-27 22:38:45 +0800

    Header + Sidebar

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="h-12 bg-blue-100 flex justify-between items-center px-5">
+      <div>Logo</div>
+      <div>Connect wallet</div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import { ReactComponent as MarketLogo } from "../assets/market.svg";
+import { ReactComponent as PencilLogo } from "../assets/pencil.svg";
+
+export default function Sidebar() {
+  return (
+    <div
+      className="fixed flex flex-col bg-blue-600 rounded-r-xl w-16 top-20 pt-5"
+      style={{
+        height: "calc(100vh - 3rem - 2 * 2rem)",
+      }}
+    >
+      <div
+        className="bg-blue-50 rounded-l-lg h-12 p-2 flex justify-center items-center"
+        style={{
+          marginLeft: "20%",
+          paddingRight: "calc(20% + 0.5rem)",
+        }}
+      >
+        <PencilLogo />
+      </div>
+      <div
+        className="rounded-l-lg h-12 p-2 flex justify-center items-center"
+        style={{ marginLeft: "20%", paddingRight: "calc(20% + 0.5rem)" }}
+      >
+        <MarketLogo />
+      </div>
+    </div>
+  );
+}
