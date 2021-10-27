d2df30faea2cfdf2aa619116971427484692ee46
Tejas Kothari <tejas23682320@gmail.com>
2021-10-27 23:05:41 +0800

    Create NFT Page 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateNFT1 from "./pages/CreateNFT1";
 
 function App() {
   return (
-    <div className="h-screen">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="h-screen">
+        <Header />
+        <Sidebar />
+        <div
+          className="pl-14 mt-8"
+          style={{ height: "calc(100vh - 3rem - 2 * 2rem)" }}
+        >
+          <Switch>
+            <Route path="/" exact>
+              <CreateNFT1 />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/CreateNFT1.js
@@ -0,0 +1,7 @@
+export default function CreateNFT1() {
+  return (
+    <div className="h-full flex">
+      <div className="bg-blue-50 w-96 rounded-r-xl"></div>
+    </div>
+  );
+}
